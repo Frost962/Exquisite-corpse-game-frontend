1a8b9915fe9651c815363e2654b80a5735733258
Mohammad Aljariri <moe.jariri@gmail.com>
2023-06-06 12:53:47 +0200

    creat

--- src/components/CreateStory.jsx
+++ src/components/CreateStory.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-function CreateStory() {
-  return <div>CreateStory</div>;
-}
+const CreateStory = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const fd = new FormData();
+    fd.append("title", title);
+    fd.append("content", content);
+
+    try {
+      const response = await axios.post("/api/stories", fd, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="title"
+        placeholder="Story Title"
+        required
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <textarea
+        name="content"
+        placeholder="Story Content"
+        required
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <button type="submit">Create Story</button>
+    </form>
+  );
+};
 
 export default CreateStory;
