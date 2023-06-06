43e6437567229603e88ce44c7dfef04101646aa3
Mohammad Aljariri <moe.jariri@gmail.com>
2023-06-06 11:14:17 +0200

    navBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 import { useContext } from "react";
-import { authContext } from "../context/authContext";
+import { AuthContext } from "../context/authContext";
 import "./Navbar.css";
+import { NavLink } from "react-router-dom";
 
-function navBar() {
-  return <div>navBar</div>;
-}
+const Navbar = () => {
+  const { user, isLoggedIn, authenticateUser } = useContext(AuthContext);
+  console.log(user);
+  const logout = () => {
+    localStorage.removeItem("authToken");
+    authenticateUser();
+  };
+  return (
+    <nav className="Navbar">
+      <ul>
+        <li>
+          <NavLink to={"/"}>Home</NavLink>
+        </li>
+        {!isLoggedIn && (
+          <>
+            <li>
+              <NavLink to={"/auth/login"}>Log in</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/auth/signup"}>Sign up</NavLink>
+            </li>
+          </>
+        )}
 
-export default navBar;
+        <>
+          <li>
+            <button onClick={logout}>Logout</button>
+          </li>
+        </>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
