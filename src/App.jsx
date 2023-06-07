0c41a06212959ba0e4a6d8de1cdcf407ae79c5c9
Mohammad Aljariri <moe.jariri@gmail.com>
2023-06-07 14:57:02 +0200

    routes2nd

--- src/App.jsx
+++ src/App.jsx
@@ -10,36 +10,39 @@ import AllStories from "./pages2/AllStories";
 import CreateStory from "./pages2/CreateStory";
 import OneStory from "./pages2/OneStory";
 import FavoriteStory from "./pages2/FavoriteStory";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Layout from "./components/Layout";
+import ParticipantsStories from "./pages2/ParticipantsStories";
 
 function App() {
   return (
-    <AuthContextWrapper>
-      <Router>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Homepage />}></Route>
-            <Route element={<IsAdmin />}></Route>
-            <Route element={<ProtectedRoute />}>
-              <Route path="/allstories" element={<AllStories />} />
-              <Route path="/creatstory" element={<CreateStory />} />
-              <Route path="/onestory" element={<OneStory />} />
-              <Route path="/favoritestory" element={<FavoriteStory />} />
-              <Route
-                path="/participantsstories"
-                element={<ParticipantsStories />}
-              />
-            </Route>
+    <>
+      <AuthContextWrapper>
+        <Router>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Homepage />}></Route>
 
-            <Route path="/auth">
-              <Route path="login" element={<authForm mode="Log in" />} />
-              <Route path="signup" element={<authForm mode="Signup" />} />
-            </Route>
+              <Route element={<ProtectedRoute />}>
+                <Route path="/allstories" element={<AllStories />} />
+                <Route path="/creatstory" element={<CreateStory />} />
+                <Route path="/onestory" element={<OneStory />} />
+                <Route path="/favoritestory" element={<FavoriteStory />} />
+                <Route
+                  path="/participantsstories"
+                  element={<ParticipantsStories />}
+                />
+              </Route>
 
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </Router>
-    </AuthContextWrapper>
+              <Route path="/auth">
+                <Route path="login" element={<authForm mode="Log in" />} />
+                <Route path="signup" element={<authForm mode="Signup" />} />
+              </Route>
+            </Route>
+          </Routes>
+        </Router>
+      </AuthContextWrapper>
+    </>
   );
 }
 
