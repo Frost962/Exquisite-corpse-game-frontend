3e0c8d5c3616c6eb560148ef3044a518116596e3
Paul Charousset <pacharousset@gmail.com>
2023-06-08 17:31:47 +0200

    faefa

--- src/pages2/Homepage.jsx
+++ src/pages2/Homepage.jsx
@@ -3,9 +3,20 @@ import Navbar from "../components/NavBar";
 
 function Homepage() {
   return (
-    <>
-      <div>welcome!!</div>
-    </>
+    <div className="homepage-container">
+      <h1>Welcome to Coop Writing!</h1>
+      <div className="homepage-content">
+        <p>
+          Coop Writing is a place for you to write community-shared stories.
+          Create a Story, add its first chapter(s) and let other people from
+          across the world take over!
+        </p>
+        <p>
+          Here stories are eternal but certainly not unchangeable. You can add a
+          chapter to anyone's story!
+        </p>
+      </div>
+    </div>
   );
 }
 
